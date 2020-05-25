22b5da6d670704a31ade4ae06a012503fdade0b5
sjain <jsapan4@gmail.com>
2020-05-25 15:49:38 -0500

    first commit

---
+++ src/main/java/com/jlabs/service/PatientService.java
@@ -0,0 +1,22 @@
+package com.jlabs.service;
+
+import com.jlabs.model.Patient;
+import org.springframework.stereotype.Service;
+
+/**
+ * Created by sjain on 5/25/20.
+ */
+
+@Service
+public class PatientService {
+
+    public String savePatient(Patient patient){
+        return null;
+    }
+
+    public Patient searchPatient(String name) {
+
+
+        return null;
+    }
+}
